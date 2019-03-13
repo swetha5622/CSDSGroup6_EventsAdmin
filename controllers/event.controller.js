ec30e7f20b46f6c371e3f3ffc0edc23f8500a326
swetha5622 <swetha5622@live.com>
2019-03-12 21:18:46 -0400

    added list events webservice

--- controllers/event.controller.js
+++ controllers/event.controller.js
@@ -4,12 +4,104 @@ const Mailer = require('../utils/mailer');
 const Event = require('../models/event.model');
 const RecurringPattern = require('../models/recurringpattern.model');
 
+const EventProperties = ['title','description','location','startdate',
+'enddate','starttime','endtime','isfulldayevent','isrecurring',
+'organizername','organizeremail','maxattendees'];
+
+const RecurringEventProperties = ['recurring_type','separation_count',
+'max_num_occurences','day_of_week','week_of_month','day_of_month','month_of_year'];
+
 /**
  * Simple test function
  */
 exports.test = function (req, res) {
     res.send('Greetings from the Northeastern University ' +
     '- CSDS Events Admin! Powered by Group 6 - 2019');
+};
+
+/**
+ * Delete Event
+ * 
+ * Removes the event from the csdsevents table,
+ * and if the event is recurring, removes the 
+ * entry from the recurring pattern table as well.
+ */
+// exports.deleteEvent = function(req, res) {
+//     Event.findByIdAndRemove(
+//         req.params.id,
+//         function(err) {
+//             if (err) return next(err);
+//             res.send('Event deleted successfully');
+//         }
+//     );
+// };
+
+/**
+ * List all events.
+ * If any event is a recurring, display the
+ * recurrence details as well.
+ */
+exports.listEvents = function(req, res) {
+
+    Event.find({}, function(err, events) {
+
+        if (err) {
+            return next(err);
+        }
+
+        var listResponse = {};
+        if (Array.isArray(events) && events.length > 0) {
+
+            var recurringEventIdList = [];
+
+            // read all the generic event properties
+            for (let i=0; i<events.length; i++){
+                if (events[i].isrecurring === true) {
+                    recurringEventIdList.push(events[i].id);
+                }
+                let e = {};
+                listResponse[events[i].id] = {};
+                EventProperties.forEach(function(p){
+                    listResponse[events[i].id][p] = events[i][p];
+                }, this);
+            }
+
+            // filter all the events that are recurring
+            // and read the recurrence properties
+            if (recurringEventIdList.length > 0) {
+
+                // returns all document with the given event ids
+                RecurringPattern.find(
+                    { 'event_id': { $in: recurringEventIdList } },
+                    function(err, recurringEventDetails) {
+
+                        if (err) {
+                            return next(err);
+                        }
+
+                        if (Array.isArray(recurringEventDetails) && recurringEventDetails.length > 0) {
+                            for (let j=0; j<recurringEventDetails.length; j++) {
+                                RecurringEventProperties.forEach(function(p) {
+                                    listResponse[recurringEventDetails[j]['event_id']][p] = 
+                                    recurringEventDetails[j][p];
+                                }, this);
+                            }
+                        }
+                        res.send({events: listResponse});
+                    }
+                )
+
+            } else {
+
+                res.send({events: listResponse});
+
+            }
+        } else {
+
+            res.send({events: listResponse});
+
+        }
+     });
 };
 
 /**
--- routes/event.route.js
+++ routes/event.route.js
@@ -6,8 +6,12 @@ const event_controller = require('../controllers/event.controller');
 // GET Requests
 router.get('/test', event_controller.test);
 router.get('/createevent', event_controller.renderCreateEventForm);
+router.get('/listevents', event_controller.listEvents);
 
 // POST Requests
 router.post('/create', event_controller.createNewEvent);
+
+// DELETE Requests
+// router.delete('/:id/delete', event_controller.deleteEvent);
 
 module.exports = router;
